00389efbd82f1d62061f0c8b7398485ab679cc01
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-11-17 14:58:17 +0200

    Add PrimaryKey annotation to AuthorEntity id
    Annotated the 'id' field in AuthorEntity with @PrimaryKey(autoGenerate = true) to ensure Room recognizes it as the primary key and auto-generates its value. This improves entity definition and database integrity.

--- app/src/main/java/com/example/libraryapp_lab4/data/local/entity/AuthorEntity.kt
+++ app/src/main/java/com/example/libraryapp_lab4/data/local/entity/AuthorEntity.kt
@@ -2,12 +2,14 @@ package com.example.libraryapp_lab4.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.Index
+import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "authors",
     indices = [Index(value = ["name"], unique = true)]
 )
 data class AuthorEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0, // Default value is needed for auto-generation
     val name: String
 )
